e0838c0b85b9893babd1ac5cc455a1f7e8d9a8f9
Atsushi Kikuchi <nzlhl1d19@gmail.com>
2021-06-14 12:23:47 +0900

    add:extract_key

---
+++ etherKeys/confirm_address.js
@@ -0,0 +1,5 @@
+const privateKeyToAddress = require('ethereum-private-key-to-address')
+
+// console.log(privateKeyToAddress(Buffer.from('', 'hex'))) //
+// console.log(privateKeyToAddress('')) //
+console.log(privateKeyToAddress('')) //
